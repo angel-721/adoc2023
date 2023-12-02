be0028f4524e99ec1a02df48aadba3a51896c44b
angel-721 <angelvelasquez2002@gmail.com>
2023-12-02 01:05:34 -0700

    Day 2 part 2 done

--- day-two/day2.nim
+++ day-two/day2.nim
@@ -51,14 +51,25 @@ proc is_possible_game_part_one(file_path: string): void =
     echo ids_combined
     f.close()
 
+proc is_possible_game_part_two(file_path: string): void =
+    let f: File = open(file_path)
+    let file_lines: string = readAll(f)
+    var powers:int = 0
+    var id:int = 0
+    var line_array: seq[string]
+    var word: string
+    var line_is_good:bool
+    var power:int = 0
+
     for line in file_lines.splitLines():
         line_is_good = true
         echo line
         line_array = line.split(" ")
         id = parseInt(line_array[1].substr(0, line_array[1].len-2))
-        echo id
 
         var color_numbers = {"red": 0, "green": 0, "blue": 0}.newTable 
+
+        var color_numbers_max = {"red": 0, "green": 0, "blue": 0}.newTable 
         for i in countup(0, line_array.len-1):
             word = line_array[i]
 
@@ -70,22 +81,20 @@ proc is_possible_game_part_one(file_path: string): void =
                 color_numbers["blue"] += parseInt(line_array[i-1])
                 
             if word[word.len-1] == ';' or i == line_array.len-1:
-                echo color_numbers
-                if color_numbers["red"] > allowed_numbers["red"]:
-                    line_is_good = false
-                if color_numbers["green"] > allowed_numbers["green"]:
-                    line_is_good = false
-                if color_numbers["blue"] > allowed_numbers["blue"]:
-                    line_is_good = false
+                if color_numbers["red"] > color_numbers_max["red"]:
+                    color_numbers_max["red"] = color_numbers["red"]
+                if color_numbers["green"] > color_numbers_max["green"]:
+                    color_numbers_max["green"] = color_numbers["green"]
+                if color_numbers["blue"] > color_numbers_max["blue"]:
+                    color_numbers_max["blue"] = color_numbers["blue"]
 
                 color_numbers["red"] = 0
                 color_numbers["green"] = 0
                 color_numbers["blue"]  = 0
-                
-        if line_is_good:
-            ids_combined += id
-    
-    echo ids_combined
+        # echo color_numbers_max 
+        power = color_numbers_max["red"] * color_numbers_max["green"] * color_numbers_max["blue"]
+        powers += power
+    echo powers
     f.close()
 
-is_possible_game_part_one("input2.txt")
+is_possible_game_part_two("input2.txt")
